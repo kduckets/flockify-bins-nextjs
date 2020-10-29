2e8a1ba786b84d45b80a537aec3c97c4574311b2
Kev <kmditroia@gmail.com>
2020-10-29 12:23:45 -0400

    record page

--- components/recordBin.js
+++ components/recordBin.js
@@ -3,12 +3,17 @@ import { Tabs, Tab } from "react-bootstrap";
 import styles from '../styles/Home.module.css'
 import { animated, useSpring } from "react-spring";
 import { useScroll } from "react-use-gesture";
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import { motion } from 'framer-motion';
 
 
 export default function Bin({ data }) {
+    
     const [style, set] = useSpring(() => ({
         transform: "perspective(500px) rotateY(0deg)"
       }));
+
       const bind = useScroll(event => {
         set({
           transform: `perspective(500px) rotateY(${
@@ -16,33 +21,51 @@ export default function Bin({ data }) {
           }deg)`
         });
       });
+
+   
       if(data.length == 0)
         return (
-            <div>
-                <p>Loading...</p>
-            </div>
+          <Container>
+          <Spinner animation="grow" variant="info" />
+          </Container>
         )
 
     return (
  <div>
     <Tabs defaultActiveKey={data[data.length-1]} id="noanim-tab-example" >
-        <Tab eventKey={data[data.length-1]} title={data[data.length-1]}>
-            <div className={styles.container} {...bind()}>
-     
+        <Tab eventKey={data[data.length-1]} title={data[data.length-1]}>       
+            <div className={styles.container} {...bind()}> 
              {data.slice(0,100).map(post => 
+              <motion.div initial="hidden" animate="visible" variants={{
+                hidden: {
+                scale: .8,
+                opacity: 0
+                },
+                 visible: {
+                  scale: 1,
+                   opacity: 1,
+               transition: {
+                   delay: .4
+                  }
+                  },
+              }}>
                <Link href="/record/[id]" as={'/record/' + post[0]} key={post[0]}>
                  <a>
+       
                <animated.div style={style}>
                 <img 
                     src ={post[1] !== undefined? post[1].image_medium : ''} 
                     className = {styles.post}
                 />
                 </animated.div>    
+               
                 </a> 
             </Link>
+            </motion.div>
         )}   
       </div>
-      </Tab>
+    
+      </Tab> 
   </Tabs>
         </div>
     )
--- pages/record/[id].js
+++ pages/record/[id].js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react';
 import fire from '../../config/fire-config';
+import styles from '../../styles/Home.module.css'
 import Link from 'next/link'
+import Button from '@material-ui/core/Button';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import Image from 'react-bootstrap/Image';
+import { makeStyles } from '@material-ui/core/styles';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
 const Record = (props) => {
   const [record, setRecord] = useState(null);  useEffect(() => {
@@ -11,22 +19,124 @@ const Record = (props) => {
         setRecord(result.val())
       })
   }, []);  
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    width: '50rem',
+  },
+  image: {
+    position: 'relative',
+     height: '50rem',
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
+}));
+
+
+const classes = useStyles();
   
   if(!record){
     return(
-      <h2>Loading...</h2>
+      <Container>
+      <Spinner animation="grow" variant="info" />
+      </Container>
     )
   }  return (
-    <div>
-      <h2>{record.media_info.album}</h2>
-      <p>
-        {record.media_info.artist}
-      </p>
-      <Link href="/">
-        <a>Back</a>
-      </Link>
+    <div className={classes.root}>
+        <ButtonBase
+          href={"https://duckduckgo.com/?q=!ducky+"+record.media_info.artist.replace(/ /g, '+') + "band+website"}
+          target="_blank"
+          focusRipple
+          key={record.media_info.album}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: '100%',
+          }}
+        >
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${record.image_medium})`,
+            }}
+          />
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {"Buy Record From Artist's Website"}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase>
     </div>
-  )
+  );
 }
 Record.getInitialProps = ({ query }) => {
   return {
